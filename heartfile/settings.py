698c83e2417d3bc3a1ccdc39bdbaa1243ac225d2
DeepaSakthivel <deepasakthivel115@gmail.com>
2021-04-04 15:59:18 +0530

    edit

--- heartfile/settings.py
+++ heartfile/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6=xs^aju8u$++^*n+a#or&++75%v6b+*l-h7xr-+=r$!4uafs4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.222.158.201','localhost']
+ALLOWED_HOSTS = ['3.22.216.165','localhost']
 
 
 # Application definition
